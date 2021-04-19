59090c820a84b9ea6d724703fa844fc1971728bd
Prateek Gumasta <PGumasta@moreyeahs.co>
2021-04-19 16:36:20 +0530

    added test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
+describe('App component', () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  })
+
+  it('starts with a count of 0', () => {
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 0');
+  });
+
+  it('increments count by 1 when the increment button is clicked', () => {
+    wrapper.setProps({'test': 'value'});
+    const incrementBtn = wrapper.find('button.increment');
+    incrementBtn.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 1');
+  });
+
+  it('matches the snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
